071f2142b8b91ebbf58890d83d96fa037da5043b
Frank Murphy <frank.nds@gmail.com>
2018-01-15 13:10:14 +0000

    Added Error Handling

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       console.log('Logged in ok');
     }, error => {
-      console.log('Error logging in');
+      console.log(error);
     });
 
   }
